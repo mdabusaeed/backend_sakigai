1dfa686568631b1bd7fb72ee6b47584f36870a77
mdabusaeed <abu.saeed.nicl@gmail.com>
2025-10-16 16:51:13 +0600

    Initial commit - Django backend project

---
+++ product/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from decimal import Decimal
+from product.models import Category, Product, Review
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'product_count']
+
+    product_count = serializers.IntegerField(read_only=True)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price',
+                  'stock', 'category', 'price_with_tax']  # other
+
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product):
+        return round(product.price * Decimal(1.1), 2)
+
+    def validate_price(self, price):
+        if price < 0:
+            raise serializers.ValidationError('Price could not be negative')
+        return price
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'name', 'description']
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id=product_id, **validated_data)
---
+++ sakigai/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sakigai project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sakigai.settings')
+
+application = get_wsgi_application()
